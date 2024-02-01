9038604f5f441a2922f6c67daf97924b966d2175
sunithalv <sunithalv05@gmail.com>
2024-02-01 09:14:46 +0400

    Commit latest code changes

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="clothing-apparel-detection",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Sunitha L V",
+    author_email="sunithalv05@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
